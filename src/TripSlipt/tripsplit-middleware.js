5da39030256d2f80784e30801f3233ebfe4d3741
openwell <timileyinojo@yahoo.com>
2019-07-29 14:48:05 +0100

    create RegexEmail Validtion, setup configDB and user scheme

--- src/TripSlipt/tripsplit-middleware.js
+++ src/TripSlipt/tripsplit-middleware.js
@@ -8,6 +8,11 @@ module.exports = {
   validateUserPassword
 };
 
+function RegexEmailValidation(email) {
+  const RegexEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return RegexEmail.test(String(email).toLowerCase());
+}
+
 function validateUser(req, res, next) {
   const { email, password } = req.body;
   if (!req.body) {
@@ -22,28 +27,33 @@ function validateUser(req, res, next) {
     return res.status(400).json({
       message: "missing required password field"
     });
+  } else if (!RegexEmailValidation(email)) {
+    return res.status(400).json({
+      message: "valid email required"
+    });
   }
   next();
 }
 
+
 async function validateUserPassword(req, res, next) {
-    const { username, password } = req.body;
-    try {
-      let userData = await db.getByUsername(username);
-      let compareOutput = bcrypt.compare(password, userData.password);
-      if (!compareOutput) {
-        return res.status(401).json({ error: "Incorrect Password" });
-      }
-      const Encrypted = jwt.sign(
-        {
-          exp: Math.floor(Date.now() / 1000) + 60 * 60,
-          data: userData.id
-        },
-        process.env.jwt_SECRET
-      );
-      req.user = { token: Encrypted };
-      next();
-    } catch (err) {
-      return res.status(401).json({ error: "Incorrect Email" });
+  const { email, password } = req.body;
+  try {
+    let userData = await db.getUserByEmail(email);
+    let compareOutput = bcrypt.compare(password, userData.password);
+    if (!compareOutput) {
+      return res.status(401).json({ error: "Incorrect Password" });
     }
+    const Encrypted = jwt.sign(
+      {
+        exp: Math.floor(Date.now() / 1000) + 60 * 60,
+        data: userData.id
+      },
+      process.env.jwt_SECRET
+    );
+    req.user = { token: Encrypted };
+    next();
+  } catch (err) {
+    return res.status(401).json({ error: "Incorrect Email" });
   }
+}
