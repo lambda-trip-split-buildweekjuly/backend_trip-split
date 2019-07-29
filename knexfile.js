9092e30a4602c1125c6799f83bdb47a8e180e9f7
openwell <timileyinojo@yahoo.com>
2019-07-29 13:29:44 +0100

    setup of knex and file structure

---
+++ knexfile.js
@@ -0,0 +1,62 @@
+module.exports = {
+  development: {
+    client: "sqlite3",
+    useNullAsDefault: true,
+    connection: {
+      filename: "./src/data/split-trip.db3"
+    },
+    migrations: {
+      directory: "./src/data/migrations"
+    },
+    seeds: {
+      directory: "./src/data/seeds"
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run("PRAGMA foreign_keys = ON", done);
+      }
+    }
+  },
+  testing: {
+    client: "sqlite3",
+    connection: { filename: "./database/test-split-trip.db3" },
+    useNullAsDefault: true,
+    migrations: {
+      directory: "./database/migrations",
+      tableName: "dbmigrations"
+    },
+    seeds: { directory: "./database/seeds" }
+  },
+
+  staging: {
+    client: "postgresql",
+    connection: {
+      database: "my_db",
+      user: "username",
+      password: "password"
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: "knex_migrations"
+    }
+  },
+
+  production: {
+    client: "postgresql",
+    connection: {
+      database: "my_db",
+      user: "username",
+      password: "password"
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: "knex_migrations"
+    }
+  }
+};
